a9da4bc3306bc7f57fb2399e6003ab12cde060f1
Sam Ma <shalahu@163.com>
2014-03-22 23:25:19 +0800

    1、将Senparc.Weixin.MP.dll单独作为插件共享出来
    2、添加扩展点的有效性验证
    3、移除build出的文件并加入.gitignore列表

--- UIShell.WeChatShell/Plugins/UIShell.WeChatProxyPlugin/WeChatProxyContainer.cs
+++ UIShell.WeChatShell/Plugins/UIShell.WeChatProxyPlugin/WeChatProxyContainer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using UIShell.OSGi.Utility;
 
 namespace UIShell.WeChatProxyPlugin
 {
@@ -11,6 +12,7 @@ namespace UIShell.WeChatProxyPlugin
         private const string NAME_ATTRIBUTE = "Name";
         private const string TOKEN_ATTRIBUTE = "Token";
         private const string HANDLER_ATTRIBUTE = "Handler";
+
         public List<WeChatProxy> WeChatProxies = new List<WeChatProxy>();
 
         public WeChatProxyContainer()
@@ -18,8 +20,39 @@ namespace UIShell.WeChatProxyPlugin
             var extensions = Activator.ExtensionService.GetExtensionProviders(WECHAT_PROXY);
             foreach (var extension in extensions)
             {
-                WeChatProxies.Add(new WeChatProxy() { Name = extension.AttributesCollection[NAME_ATTRIBUTE], Token = extension.AttributesCollection[TOKEN_ATTRIBUTE], Handler = extension.AttributesCollection[HANDLER_ATTRIBUTE], Bundle = extension.Bundle });
+                string name = extension.AttributesCollection[NAME_ATTRIBUTE];
+                string token = extension.AttributesCollection[TOKEN_ATTRIBUTE];
+                string hanlder = extension.AttributesCollection[HANDLER_ATTRIBUTE];
+                string symbolicName = extension.Bundle.SymbolicName;
+
+                if (Validate(hanlder, HANDLER_ATTRIBUTE, symbolicName) && Validate(token, TOKEN_ATTRIBUTE, symbolicName))
+                {
+                    WeChatProxies.Add(new WeChatProxy() { Name = name, Token = token, Handler = hanlder, Bundle = extension.Bundle });
+                }
             }
+        }
+
+        private bool Validate(string value, string name, string symbolicName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                FileLogUtility.Error(string.Format("{0} is not specified in bundle {1} for extension point {2} so it's ignored.", name, symbolicName, WECHAT_PROXY));
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool Validate(ref string value, string name, string symbolicName, string defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                value = defaultValue;
+                FileLogUtility.Error(string.Format("{0} is not specified in bundle {1} for extension point {2} so it's set to default {3}.", name, symbolicName, WECHAT_PROXY, defaultValue));
+            }
+
+            return true;
         }
     }
 }
